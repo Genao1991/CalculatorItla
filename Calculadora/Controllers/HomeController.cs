2630bf4bcef22fe719dafef89c83ad815a6d086a
Genao1991 <anthonygen91@gmail.com>
2018-07-22 20:58:03 -0400

    Primer commit

---
+++ Calculadora/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Web.Mvc;
+
+namespace Calculadora.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index(string answer)
+        {
+
+            return View("Index");
+        }
+    }
+}
